d1f5f5ccc74ad260799fdd7e123bf64fc082ea58
Minh Đức <duc.trinh@codeart.vn>
2023-06-07 15:56:55 +0700

    Update Purchase Reports

--- PURCHASE/purchase-reports/purchase-reports.page.ts
+++ PURCHASE/purchase-reports/purchase-reports.page.ts
@@ -25,7 +25,7 @@ export class PurchaseReportsPage extends PageBase {
         toDate: '',
         buyer: null,
         vendor: null,
-        isCalcShippedOnly: true,
+        isCalcShippedOnly: false,
     }
 
     constructor(
